db2653d1e1006e4d7c9f297077eeb7ff857d95ca
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-07-12 15:15:03 +0300

    BAP-22123: Integrate Rector to simplify projects upgrade (#36035)

--- sets/oro-51/cron-commands.php
+++ sets/oro-51/cron-commands.php
@@ -7,6 +7,6 @@ use Rector\Transform\Rector\Class_\MergeInterfacesRector;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ImplementCronCommandScheduleDefinitionInterfaceRector::class);
     $rectorConfig->ruleWithConfiguration(MergeInterfacesRector::class, [
-        'Oro\Bundle\CronBundle\Command\CronCommandInterface' => 'Oro\Bundle\CronBundle\Command\CronCommandActivationInterface',
+        'Oro\Bundle\CronBundle\Command\CronCommandInterface' => '\Oro\Bundle\CronBundle\Command\CronCommandActivationInterface',
     ]);
 };
