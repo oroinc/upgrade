7b058eaa2c24a14e325fecae0f34c3bd6a6c0241
Yurii Muratov <ymuratov@oroinc.com>
2024-08-21 19:34:36 +0300

    BB-23720: Conversation as activity in back-office (#39169)
    - remove dependency on non published package

--- sets/skip-list.php
+++ sets/skip-list.php
@@ -3,38 +3,10 @@
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    // Skip vendors, node_modules, etc. in the processed directory
-    $skipList = [
+    // Skip vendors, node_modules, etc
+    $rectorConfig->skip([
         '*/vendor/*',
         '*/node_modules/*',
-        '*/var/cache/*',
-    ];
-
-    if (
-        'cli' === php_sapi_name()
-        && ini_get('register_argc_argv')
-        && array_key_exists('argv', $_SERVER)
-    ) {
-        $args = $_SERVER['argv'];
-        if ($i = array_search('process', $args)) {
-            $args = array_slice($args, $i + 1);
-            foreach ($args as $argument) {
-                if (str_starts_with($argument, '--')) {
-                    break;
-                }
-
-                if (is_dir(realpath("./$argument"))) {
-                    $processedDir = realpath("./$argument");
-
-                    $skipList = [
-                        "$processedDir*/vendor/*",
-                        "$processedDir*/node_modules/*",
-                        "$processedDir*/var/*",
-                    ];
-                }
-            }
-        }
-    }
-
-    $rectorConfig->skip($skipList);
+        '*/var/*',
+    ]);
 };
