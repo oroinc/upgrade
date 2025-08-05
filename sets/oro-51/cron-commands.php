8555f5a74ed0f25473efcd943ad7acf48346be03
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2025-08-05 22:55:29 +0300

    BAP-23133: Upgrade toolkit. Fix reported issues (#42228)
    - Fixed issue with double slash insertion before CronCommandActivationInterface
    - Replaced MergeInterfacesRector with RenameClassRector to provide a more strict replacement and avoid double adding of CronCommandScheduleDefinitionInterface

--- sets/oro-51/cron-commands.php
+++ sets/oro-51/cron-commands.php
@@ -2,11 +2,11 @@
 
 use Oro\UpgradeToolkit\Rector\Rules\Oro51\ImplementCronCommandScheduleDefinitionInterfaceRector;
 use Rector\Config\RectorConfig;
-use Rector\Transform\Rector\Class_\MergeInterfacesRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ImplementCronCommandScheduleDefinitionInterfaceRector::class);
-    $rectorConfig->ruleWithConfiguration(MergeInterfacesRector::class, [
-        'Oro\Bundle\CronBundle\Command\CronCommandInterface' => '\Oro\Bundle\CronBundle\Command\CronCommandActivationInterface',
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Oro\Bundle\CronBundle\Command\CronCommandInterface' => 'Oro\Bundle\CronBundle\Command\CronCommandActivationInterface',
     ]);
 };
