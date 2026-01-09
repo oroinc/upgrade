e333337818f9ad5ef252ee7b8556351f502a3894
Andrii Yatsenko <ayatsenko@oroinc.com>
2026-01-09 19:33:23 +0200

    BAP-23109: Upgrade Symfony to 7.4, Doctrine DBAL to 3.10 and PHP requirement to 8.5 (#42767)
    - Upgrade Symfony to 7.4
    - Upgrade Doctrine ORM to 2.20.9 and DBAL to 3.10.4
    - Update Monolog to 3.9, Twig to 3.21, MongoDB to 2.1.2
    - Upgrade Gedmo doctrine-extensions to v3.21
    - Update PHP requirement to 8.5, Node.js to 24
    
    ---------
    Co-authored-by: OleksandrProtsiuk <oprotsiuk@oroinc.com>
    Co-authored-by: Vitalii Yeromenko <vyeromenko@oroinc.com>
    Co-authored-by: Vadim Malesh <vmalesh@oroinc.com>

--- sets/oro-70.php
+++ sets/oro-70.php
@@ -1,8 +1,15 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/skip-list.php');
     $rectorConfig->import(__DIR__ . '/oro-70/symfony.php');
+
+    // Use Oro\Component\Testing\Logger\TestLogger
+    // instead of removed Psr\Log\Test\TestLogger
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'Psr\\Log\\Test\\TestLogger' => 'Oro\\Component\\Testing\\Logger\\TestLogger',
+    ]);
 };
